9fe86440546310a03295596f2273568034e2d2d3
admin <admin@admins-iMac.local>
2018-02-14 12:09:42 +0800

    db update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-const mLab = 'mongodb://<test>:<test>@ds149905.mlab.com:49905/todoappapi'
+const mLab = 'mongodb://test:test@ds149905.mlab.com:49905/todoappapi'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mLab || 'mongodb://localhost:27017/TodoApp');
