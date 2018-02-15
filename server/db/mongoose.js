c4f6cb34efdf099bf6f42bdacbed846ecabb67e3
admin <admin@admins-iMac.local>
2018-02-15 14:18:56 +0800

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 const mLab = 'mongodb://test:test@ds149905.mlab.com:49905/todoappapi'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mLab || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || mLab);
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
